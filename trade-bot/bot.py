72df70da7b070918d4e47eef0fe363cafd508400
ubuntu <vedarsh@hotmail.com>
2021-10-08 21:04:28 -0700

    added documentation

--- trade-bot/bot.py
+++ trade-bot/bot.py
@@ -1,9 +1,10 @@
+#ml functions
 import math
 import numpy
 import plotly
 import asyncio
 
-
+#api functions
 import yfinance as yf
 from time import sleep
 import plotly.graph_objects as go
@@ -13,19 +14,22 @@ class TradeInit:
         #initialising class elements
         self.eval_time = eval_time
         self.ticker = ticker
+            #initialising yf api ticker
         self.tk = yf.Ticker(ticker)
+            #default sleep time is 2.5s
         if eval_time is None:
             eval_time = 2500
     
     @property
     def information(self):
+        #returns stock information including history
         return self.tk.info
     
     @property
     def max_volume(self):
+        #returns total history for max time period
         return self.tk.history(period = "max")
 
-
 if __name__ == "__main__":
-    tick = TradeInit("TSLA")
-    print(tick.max_volume)
+    tick = TradeInit("TSLA") #placeholder "TSLA"
+    print(tick.max_volume)   #returns max volume
