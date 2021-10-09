6bfddde73cee2ac7730430ea8a86c7ad6607080f
ubuntu <vedarsh@hotmail.com>
2021-10-08 22:52:12 -0700

    added multiple input tokens/ multiple outputs

--- trade-bot/process.py
+++ trade-bot/process.py
@@ -1,5 +1,28 @@
 from api_init import *
 
-stonk = TradeInit("msft")
-print(stonk.max_volume)
-print(stonk.information)
+import numpy as np
+
+
+def inpt(*args, **kwargs):
+    #inputting required arguments
+    return args
+
+def init():
+    #taking multiple inputs
+    input_arr = input("give your ipla ticker:")
+    stonk_arr = inpt(list(map(str,input_arr.split(' '))))
+    #converting inputs into arrays
+    for i in range(len(stonk_arr)):
+        #secondary array
+        x = stonk_arr[i]
+        for j in range(len(x)):
+            #primary array
+            y = x[j]
+            stock = TradeInit(y)
+            print(stock.max_volume)
+            print(stock.information)
+    else:
+        return "loop_error"
+
+if __name__ == "__main__":
+    init()
