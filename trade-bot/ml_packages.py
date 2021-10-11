4794968f82aed79ac2530e9c989e34b80fec6be5
ubuntu <vedarsh@hotmail.com>
2021-10-10 23:33:48 -0700

    first algo implementation

---
+++ trade-bot/ml_packages.py
@@ -0,0 +1,17 @@
+import numpy as np
+import array as arr
+
+from api_init import *
+from process import *
+
+init()
+
+class TableMan:
+    def __init__(self, inpt, timep = None):
+        self.inpt = TradeInit(a)
+        self.timep = None
+        if self.timep is none:
+            timep = "1mo"
+    
+
+
